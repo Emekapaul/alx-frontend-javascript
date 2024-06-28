e58afa8e37c41647a523751d3c3af748f249339e
Paul Nnaemeka <emekapaul302@gmail.com>
2024-06-28 01:27:07 +0100

    Resolving tasks 0-10 0x01-ES6_promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-/* eslint-disable no-unused-vars */
-  return promise.then((result) => ({ status: 200, body: 'success' }))
-    .catch((error) => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
-/* eslint-enable no-unused-vars */
+  return promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,8 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const promise1 = uploadPhoto();
-  const promise2 = createUser();
-  return Promise.all([promise1, promise2])
-    .then((results) => {
-      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-    })
-    .catch(() => { console.log('Signup system offline'); });
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([response1, response2]) => {
+      console.log(response1.body, response2.firstName, response2.lastName);
+    }).catch(() => console.log('Signup system offline'));
 }
